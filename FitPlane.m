8b73ae488e23b77b567b1c2d14e8cb8e5acf37ef
lbw <13032652089@163.com>
2024-07-22 11:05:11 +0800

    完成平面拟合

---
+++ FitPlane.m
@@ -0,0 +1,38 @@
+function [fitresult, gof] = FitPlane(x, y, z)
+%CREATEFIT(X,Y,Z)
+%  创建一个拟合。
+%
+%  要进行 '无标题拟合 1' 拟合的数据:
+%      X 输入: x
+%      Y 输入: y
+%      Z 输出: z
+%  输出:
+%      fitresult: 表示拟合的拟合对象。
+%      gof: 带有拟合优度信息的结构体。
+%
+%  另请参阅 FIT, CFIT, SFIT.
+
+%  由 MATLAB 于 22-Jul-2024 10:46:57 自动生成
+
+
+%% 拟合: '无标题拟合 1'。
+[xData, yData, zData] = prepareSurfaceData( x, y, z );
+
+% 设置 fittype 和选项。
+ft = fittype( 'poly11' );
+
+% 对数据进行模型拟合。
+[fitresult, gof] = fit( [xData, yData], zData, ft );
+
+% 绘制数据拟合图。
+figure( 'Name', '海底平面拟合图 1' );
+h = plot( fitresult, [xData, yData], zData );
+legend( h, '拟合平面 1', 'z vs. x, y', 'Location', 'NorthEast', 'Interpreter', 'none' );
+% 为坐标区加标签
+xlabel( 'x', 'Interpreter', 'none' );
+ylabel( 'y', 'Interpreter', 'none' );
+zlabel( 'z', 'Interpreter', 'none' );
+grid on
+view( -15.0, 18.6 );
+
+
--- point2line.m
+++ point2line.m
@@ -1,7 +1,36 @@
-%%根据范围中的点进行平面拟合，并且进行划线
-
-
-
+xmin=3;
+xmax=4;
+ymin=0;
+ymax=4;
+Xmin=xmin/0.02+1
+Xmax=xmax/0.02+1
+Ymin=ymin/0.02+1
+Ymax=ymax/0.02+1
+    %%根据范围中的点进行平面拟合，并且进行划线
+    % 提取 x、y、z 坐标
+    len=(Xmax-Xmax+1)*(Ymax-Ymin+1);
+    block=submarine(Xmin:Xmax,Ymin:Ymax);
+    x=zeros(1,len);
+    y=zeros(1,len);
+    z=zeros(1,len);
+    for i=1:Xmax-Xmin+1
+        for j=1:Ymax-Ymin+1
+            index=(i-1)*(Ymax-Ymin+1)+j;
+            x(index)=(j-1)*0.02;
+            y(index)=(i-1)*0.02;
+            z(index)=block(i,j);
+        end
+    end
+    
+    [r ~]=FitPlane(x,y,z);
+    r
+    c=r.p00;
+    a=r.p10;
+    b=r.p01;
+    
+    
+    
+    figure('Name','海底测线绘制')
 
 
 
